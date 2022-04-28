6dc1e019617e1941eb09816b9e387c11afd56df1
Laine Moura <lainermoura@gmail.com>
2022-04-28 17:35:58 -0300

    altera pagina de login - ainda nao loga

--- src/pages/LoginPage/LoginForm.jsx
+++ src/pages/LoginPage/LoginForm.jsx
@@ -1,41 +1,16 @@
 import React, {useState} from "react";
 import useForm from '../../hooks/useForm'
 import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../../constants/url";
-import axios from "axios";
-import { goToSignUpPage } from "../../routes/coordinator";
-import { TextClickP } from "./styled";
+import { login } from "../../services/requests.js";
 
-const LoginForm = ({setRightButtonText}) => {
+const LoginForm = ({setButton}) => {
     const [form, onChangeInput, clear] = useForm({ email: "", password: "" })
-    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate()
     
-
     const onSubmitForm = (e) => {
         e.preventDefault()
+        login(form, clear, navigate, setButton)
     }
-
-    const token = localStorage.getItem("token")
-    const login = () => {
-        setIsLoading(true);
-        axios
-            .post(`${BASE_URL}/login`, form)
-            .then((res) => {
-                localStorage.setItem("token", res.data.token);
-                clear();
-                setIsLoading(false);
-                if (token) {
-                    navigate("/restaurant")
-                } else {
-                    navigate("/login")
-                }
-                alert("Login Efetuado!");
-            })
-            .catch((err) => {
-                alert("Erro no login!");
-            });
-    };
 
 
     return(
@@ -55,7 +30,6 @@ const LoginForm = ({setRightButtonText}) => {
                 />
                 <button type={"submit"}>LOGIN</button>
                 <br/>
-                <TextClickP onClick={()=> goToSignUpPage(navigate)}>Não possui cadastro? Clique aqui!</TextClickP>
                 
             </form>
         </div>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import { goToSignUpPage } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
+import useUnprotectedPage from "../../hooks/useUnprotectedPage";
 
-const LoginPage = () => {
-  return(
-    <div>
-      LoginPage
-      <LoginForm/>
-      </div>
-  );
-};
-export default LoginPage;
+
+const LoginPage = ({setButton}) => {
+    const navigate = useNavigate()
+    useUnprotectedPage()
+
+    return (
+        <div>
+            <LoginForm setButton={setButton}/>
+            <div>
+                <button
+                    onClick={() => goToSignUpPage(navigate)}
+                    type={"submit"}
+                >
+                    Não possui cadastro? Clique aqui.
+                    </button>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage

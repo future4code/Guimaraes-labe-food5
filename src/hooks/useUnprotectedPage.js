6b910056bf8924032f887ab521b144f029545ccc
Laine Moura <lainermoura@gmail.com>
2022-05-01 10:52:56 -0300

    corrige bug que a tela pulava da Home para Feed

--- src/hooks/useUnprotectedPage.js
+++ src/hooks/useUnprotectedPage.js
@@ -1,16 +1,16 @@
+import { useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { goToSearchRestaurantPage } from "../routes/coordinator";
 
 const useUnprotectedPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-
-    if (token !== null) {
-        navigate('/home')
+  useLayoutEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      goToSearchRestaurantPage(navigate);
     }
-}, [])
+  }, [navigate]);
 };
 
 export default useUnprotectedPage;

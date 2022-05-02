d3c55f6478890dd1fe9ce15bfa346c71ca2cd0ff
CamilaCoradi <camila_coradi@hotmail.com>
2022-05-02 13:00:41 -0300

    perfil com as infos básicas, renderizado

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useProtectedPage from "../../hooks/useProtectedPage";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../../constants/url";
+
 const ProfilePage = () => {
   useProtectedPage();
-  return <div>ProfilePage</div>;
+
+  const navigate = useNavigate();
+
+  const [profileList, setProfileList] = useState([]);
+
+  const data = {
+    profileList,
+    setProfileList,
+  };
+
+  const headers = {
+    headers: {
+      auth: localStorage.getItem("token"),
+    },
+  };
+
+  useEffect(() => {
+    getProfile(data);
+  }, [profileList]);
+
+  const getProfile = () => {
+    axios
+      .get(`${BASE_URL}/profile`, headers)
+      .then((res) => {
+        setProfileList(res.data.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h3>Meu Perfil</h3>
+      <p>{profileList.name}</p>
+      <p>{profileList.email}</p>
+      <p>{profileList.cpf}</p>
+      <button>Editar Perfil</button>
+
+      <div>
+        <p>Endereço cadastrado: {profileList.address}</p>
+        <button>Editar Endereço</button>
+      </div>
+    </div>
+  );
 };
 export default ProfilePage;

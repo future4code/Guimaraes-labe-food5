08033f299c7b42d8aa2343a9b864d1e4b7c66241
Caio Porto <caioportoramos@gmail.com>
2022-05-03 21:08:53 -0300

    ainda nao esta completo

--- src/global/GlobalState.js
+++ src/global/GlobalState.js
@@ -6,18 +6,38 @@ import { goToFeedPage } from "../routes/coordinator";
 const GlobalState = (props) => {
   //Organizando o código
 
-  //1 - Hooks gerais:
-
-  //2 - Todos os estados gerais da aplicação:
+  //1 - Estados gerais da aplicação:
   const token = localStorage.getItem("token");
   const [loginButtonText, setLoginButtonText] = useState(
     token ? "Logout" : "Login"
   );
+  const [cart, setCart] = useState({});
+  const [profile, setProfile] = useState([]);
+  const [restaurantDetail, setRestaurantDetail] = useState({});
+  const [restaurant, setRestaurant] = useState([{}]);
+  const [activeOrder, setActiveOrder] = useState({});
+  const [temporaryToken, setTemporaryToken] = useState("");
 
-  const states = { loginButtonText };
-  const setters = { setLoginButtonText };
+  const states = {
+    loginButtonText,
+    cart,
+    restaurantDetail,
+    activeOrder,
+    temporaryToken,
+    profile,
+    restaurant,
+  };
+  const setters = {
+    setLoginButtonText,
+    setCart,
+    setRestaurantDetail,
+    setActiveOrder,
+    setTemporaryToken,
+    setProfile,
+    setRestaurant,
+  };
 
-  //3 - Services:
+  //2 - Services:
   const login = (body, clear, navigate, setLoginButtonText) => {
     axios
       .post(`${BASE_URL}/login`, body)
@@ -45,11 +65,125 @@ const GlobalState = (props) => {
 
   const services = { login, signUp };
 
-  //4 - Requests
+  //3 - Requests:
+  //3.1 - Restaurantes:
+  const getRestaurant = () => {
+    const token = localStorage.getItem("token");
 
+    axios
+      .get(`${BASE_URL}/restaurants`, {
+        headers: {
+          auth: token,
+        },
+      })
+      .then((res) => {
+        setRestaurants(res.data.restaurants);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+  //3.2 - Detalhes do Restaurante:
+  const getRestaurantDetails = (restaurantId) => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`${BASE_URL}/restaurants/${restaurantId}`, {
+        headers: {
+          auth: token,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setRestaurantDetail(res.data);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+  //3.3 - Adicionar objetos ao carrinho:
+
+  const addToCart = (product, quantity, restId) => {
+    const cartProduct = {};
+    cartProduct.product = product;
+    cartProduct.quantity = quantity;
+    cartProduct.restId = restId;
+    setCart([...cart, cartProduct]);
+  };
+  //3.4 - Remover Objetos do carrinho:
+  const removeFromCart = (id) => {
+    let newList = [...cart];
+    for (let i = 0; i < newList.length; i++) {
+      if (newList[i].product.id === id) {
+        if (newList.length === 1) {
+          setCart([]);
+          localStorage.removeItem("cart");
+        }
+        newList.splice(i, 1);
+        setCart(newList);
+      }
+    }
+  };
+  //3.5 - Receber Pedido:
+  const getActiveOrder = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`${BASE_URL}/active-order`, {
+        headers: {
+          auth: token,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.order);
+        setActiveOrder(res.data.order);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+  //3.6 - Receber Perfil:
+  const getProfile = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`${BASE_URL}/profile`, {
+        headers: {
+          auth: token,
+        },
+      })
+      .then((res) => {
+        setProfile(res.data.user);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+
+  const requests = {
+    addToCart,
+    removeFromCart,
+    getActiveOrder,
+    getProfile,
+    getRestaurant,
+    getRestaurantDetails,
+  };
+
+  useEffect(() => {
+    if (cart && cart.length > 0) {
+      //Caso Cart tenha conteúdo, envia para localStorage
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } else if (
+      cart.length <= 0 &&
+      localStorage.getItem("cart") &&
+      localStorage.getItem("cart").length
+    ) {
+      //Caso esteja vazio e exista cart no localStorage, recebe o que está no localstorage
+      setCart(JSON.parse(localStorage.getItem("cart")));
+    }
+  }, [cart]);
   //4 - Passar por props para o provider:
   return (
-    <GlobalStateContext.Provider value={{ states, setters, services }}>
+    <GlobalStateContext.Provider
+      value={{ states, setters, services, requests }}
+    >
       {props.children}
     </GlobalStateContext.Provider>
   );
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useCOntext, useLayoutEffect, useState } from "react";
+import GlobalStateContext from "../../global/GlobalStateContext";
 import useProtectedPage from "../../hooks/useProtectedPage";
-const CartPage = () => {
+import axios from "axios";
+import useNavigate from "react-router-dom";
+import { BASE_URL } from "../../constants/url";
+
+const CartPage = (props) => {
   useProtectedPage();
-  return <div>CartPage</div>;
+
+  const navigate = useNavigate();
+
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState(0);
+  const [total, setTotal] = useState(0);
+  const { states, setters, requests } = useContext(GlobalStateContext);
+  const { cart, activeOrder } = states;
+  const { setCart, setActiveOrder } = setters;
+  const { addToCart, removeFromCart, getActiveOrder, getProfile } = requests;
+
+  const getLocalStorage = () => {
+    //Carrinho recebe os dados dos produtos pelo local storage:
+    if (localStorage.getItem("cart") && localStorage.getItem("cart").length) {
+      setCart(JSON.parse(localStorage.getItem("cart")));
+    }
+  };
+
+  const handlePayment = (e) => {
+    setPayment(e.target.value);
+  };
+  const getShipping = (price) => {
+    setShipping(price);
+    setTotal(shipping + totalProd);
+  };
+
+  for (let i = 0; i < cart.length; i++) {
+    totalProd = cart[i].product.price * parseInt(cart[i].quantity) + totalProd;
+  }
+  /* const toBuyFoods = () => {
+    //CONTINUAR DAQUI
+    const id = cart[0].restId;
+    let body = {};
+    const result =
+      cart.length > 0 &&
+      cart.map((cart) => ({ id: cart.product.id, quantity: cart.quantity }));
+    body.products = result;
+    body.paymentMethod = payment;
+
+    const header = {
+      headers: {
+        auth: localStorage.getItem("token"),
+      },
+    };
+    axios
+      .post(`${BASE_URL}/restaurants/${id}/order`, body, header)
+      .get((res) => {
+        localStorage.removeItem("cart");
+        getLocalStorage();
+      })
+      .catch();
+  };
+ */
+  return <div></div>;
 };
 export default CartPage;

b1a7028a863c7e76d33d03582d289039238ad9be
Caio Porto <caioportoramos@gmail.com>
2022-05-04 21:16:32 -0300

    adicionado onClick ao componente ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,22 +1,49 @@
-import React from "react"
-import { Button, Container, Info } from "./styles"
-
+import React, { useContext } from "react";
+import { Button, Container, Info } from "./styles";
+import GlobalStateContext from "../../global/GlobalStateContext";
 const ProductCard = (props) => {
-    return (
-        <Container>
-            <picture>
-                <img src={props.img} alt={props.name} />
-            </picture>
-            <Info>
-                <div>
-                    <p>{props.name}</p>
-                    <span>{props.description}</span>
-                </div>
-                <p id="price">R$ {props.price.toFixed(2)}</p>
-                <Button>Adicionar</Button>
-            </Info>
-        </Container>
-    )
-}
+  const { states, setters, requests, services } =
+    useContext(GlobalStateContext);
+  const { cart } = states;
+  const { setCart } = setters;
+  //const { addToCart } = requests;
 
-export default ProductCard
+  const addItemToCart = (newItem) => {
+    //função com lógica de quantidade
+    const index = cart.findIndex((i) => i.id === newItem.id);
+    const newCart = [...cart];
+    if (index === -1) {
+      const cartItem = { ...newItem, amount: 1 };
+      newCart.push(cartItem);
+    } else {
+      //produto já está no carrinho
+      newCart[index].amount = newCart[index].amount + 1;
+    }
+    setCart(newCart);
+  };
+  return (
+    <Container>
+      <picture>
+        <img src={props.img} alt={props.name} />
+      </picture>
+      <Info>
+        <div>
+          <p>{props.name}</p>
+          <span>{props.description}</span>
+        </div>
+        <p id="price">R$ {props.price.toFixed(2)}</p>
+        <Button
+          onClick={() => {
+            addItemToCart(props.item); //Não consigo emitir alert e saber se o botão funciona
+
+            alert("Produto adicionado ao carrinho!");
+          }}
+        >
+          Adicionar
+        </Button>
+      </Info>
+    </Container>
+  );
+};
+
+export default ProductCard;
--- src/pages/RestaurantPage/RestaurantPage.jsx
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -8,112 +8,125 @@ import { BASE_URL } from "../../constants/url";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
 const RestaurantPage = () => {
-  useProtectedPage()
+  useProtectedPage();
 
-  const { restauranteId } = useParams()
+  const { restauranteId } = useParams();
 
-  const [loading, setLoading] = useState(false)
-  const [restaurant, setRestaurant] = useState({})
-  const [principal, setPrincipal] = useState([])
-  const [acompanhamento, setAcompanhamento] = useState([])
-  const [bebida, setBebida] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [restaurant, setRestaurant] = useState({});
+  const [principal, setPrincipal] = useState([]);
+  const [acompanhamento, setAcompanhamento] = useState([]);
+  const [bebida, setBebida] = useState([]);
 
   const fetch = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const { data } = await axios.get(`${BASE_URL}/restaurants/${restauranteId}`, {
-        headers: {
-          Auth: localStorage.getItem('token')
+      const { data } = await axios.get(
+        `${BASE_URL}/restaurants/${restauranteId}`,
+        {
+          headers: {
+            Auth: localStorage.getItem("token"),
+          },
         }
-      })
+      );
 
-      setRestaurant(data.restaurant)
-      setPrincipal(data.restaurant.products.filter(item => item.category !== 'Acompanhamento' && item.category !== 'Bebida'))
-      setAcompanhamento(data.restaurant.products.filter(item => item.category === 'Acompanhamento'))
-      setBebida(data.restaurant.products.filter(item => item.category === 'Bebida'))
-      setLoading(false)
+      setRestaurant(data.restaurant);
+      setPrincipal(
+        data.restaurant.products.filter(
+          (item) =>
+            item.category !== "Acompanhamento" && item.category !== "Bebida"
+        )
+      );
+      setAcompanhamento(
+        data.restaurant.products.filter(
+          (item) => item.category === "Acompanhamento"
+        )
+      );
+      setBebida(
+        data.restaurant.products.filter((item) => item.category === "Bebida")
+      );
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
 
   useEffect(() => {
-    fetch()
-  }, [])
+    fetch();
+  }, []);
 
   return (
     <Container>
       {!loading ? (
         <>
-        <Info>
-          <img src={restaurant.logoUrl} alt={restaurant.name} />
-          <p>{restaurant.name}</p>
-          <div>
-            <span>{restaurant.deliveryTime - 10} - {restaurant.deliveryTime} min</span>
-            <span>Frete R$ {restaurant.shipping}.00</span>
-          </div>
-          <span>{restaurant.address}</span>
-        </Info>
+          <Info>
+            <img src={restaurant.logoUrl} alt={restaurant.name} />
+            <p>{restaurant.name}</p>
+            <div>
+              <span>
+                {restaurant.deliveryTime - 10} - {restaurant.deliveryTime} min
+              </span>
+              <span>Frete R$ {restaurant.shipping}.00</span>
+            </div>
+            <span>{restaurant.address}</span>
+          </Info>
 
-        <Products>
-          <div id='title'>
-            <p>Principais</p>
-          </div>
-          <DisplayProducts>
-            {principal.map((item, index) => (
-            <ProductCard
-              key={index}
-              img={item.photoUrl}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-            />
-            ))}
-          </DisplayProducts>
-        </Products>
-
-        {acompanhamento.length > 0 ? (
           <Products>
-          <div id='title'>
-            <p>Acompanhamentos</p>
-          </div>
-          <DisplayProducts>
-            {acompanhamento.map((item, index) => (
-            <ProductCard
-              key={index}
-              img={item.photoUrl}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-            />
-            ))}
-          </DisplayProducts>
+            <div id="title">
+              <p>Principais</p>
+            </div>
+            <DisplayProducts>
+              {principal.map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img={item.photoUrl}
+                  name={item.name}
+                  price={item.price}
+                  description={item.description}
+                />
+              ))}
+            </DisplayProducts>
           </Products>
-        ): null}
 
-        <Products>
-          <div id='title'>
-            <p>Bebidas</p>
-          </div>
-          <DisplayProducts>
-            {bebida.map((item, index) => (
-            <ProductCard
-              key={index}
-              img={item.photoUrl}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-            />
-            ))}
-          </DisplayProducts>
-        </Products>
+          {acompanhamento.length > 0 ? (
+            <Products>
+              <div id="title">
+                <p>Acompanhamentos</p>
+              </div>
+              <DisplayProducts>
+                {acompanhamento.map((item, index) => (
+                  <ProductCard
+                    key={index}
+                    img={item.photoUrl}
+                    name={item.name}
+                    price={item.price}
+                    description={item.description}
+                  />
+                ))}
+              </DisplayProducts>
+            </Products>
+          ) : null}
 
+          <Products>
+            <div id="title">
+              <p>Bebidas</p>
+            </div>
+            <DisplayProducts>
+              {bebida.map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img={item.photoUrl}
+                  name={item.name}
+                  price={item.price}
+                  description={item.description}
+                />
+              ))}
+            </DisplayProducts>
+          </Products>
         </>
       ) : null}
-      
     </Container>
-  )
-}
+  );
+};
 
 export default RestaurantPage;
